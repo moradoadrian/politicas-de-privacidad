bbfa4d5d91bae8d0251f4bc7ba5152dff1730c62
Adrián Morado <moradoadrian@gmail.com>
2025-07-09 10:52:39 -0600

    eliminar cuenta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PoliticasDePrivacidadComponent } from './components/public/politicas-de-privacidad/politicas-de-privacidad.component';
+import { EliminarCuentaComponent } from './components/public/eliminar-cuenta/eliminar-cuenta.component';
 export const routes: Routes = [
     {path: '', redirectTo: 'politicas-de-privacidad', pathMatch: 'full'},
     {path: 'politicas-de-privacidad', component: PoliticasDePrivacidadComponent},
+    {path: 'eliminar-cuenta', component: EliminarCuentaComponent}
 ];
---
+++ src/app/components/public/eliminar-cuenta/eliminar-cuenta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-cuenta',
+  imports: [],
+  templateUrl: './eliminar-cuenta.component.html',
+  styleUrl: './eliminar-cuenta.component.scss'
+})
+export class EliminarCuentaComponent {
+
+}
